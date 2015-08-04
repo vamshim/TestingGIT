cf982b9b895825c66bd802be45b43bd7a594077b
v545586 <v545586@tusfl000vm114c8.vdsi.ent.verizon.com>
2015-08-04 12:23:40 +0530

    checkin 2

--- TestingGIT/src/com/vzbi/TestGIT.java
+++ TestingGIT/src/com/vzbi/TestGIT.java
@@ -8,6 +8,7 @@ public class TestGIT {
 	public static void main(String[] args) {
 		System.out.println(" INSIDE MAIN METHOD  ");
 		System.out.println(" INSIDE MAIN METHOD 111 ");
+		System.out.println(" INSIDE MAIN METHOD 111 ");
 	}
 
 }
